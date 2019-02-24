fc996859ed126e280ef9e57cbff175e0856a5a03
scott <scott.waye@hubse.com>
2019-02-24 13:36:08 -0500

    experimenting

--- src/Common/src/Interop/Unix/Interop.Libraries.cs
+++ src/Common/src/Interop/Unix/Interop.Libraries.cs
@@ -2,10 +2,28 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
+using System;
+
 internal static partial class Interop
 {
     internal static partial class Libraries
     {
         internal const string CoreLibNative = "System.Private.CoreLib.Native";
+    }
+}
+
+
+namespace WebAssembly
+{
+    namespace JSInterop
+    {
+        internal static class InternalCalls
+        {
+            public static bool InvokeJSUnmarshalled(out string exception, string functionIdentifier, IntPtr arg0, IntPtr arg1, IntPtr arg2)
+            {
+                exception = null;
+                return true;
+            }
+        }
     }
 }
---
+++ src/Native/Runtime/wasm/JavascriptInterop.cpp
@@ -0,0 +1,16 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+//
+// Unmanaged helpers called by the managed finalizer thread.
+//
+#include "common.h"
+
+// Fetch next object which needs finalization or return null if we've reached the end of the list.
+COOP_PINVOKE_HELPER(Boolean, WebAssembly::JSInterop::InternalCalls.InvokeJSUnmarshalled, (Object **exceptionString, Object *funcName, void *arg0, void *arg1, void *arg2))
+{
+    sdfgsfgsfdg
+    return true;
+}
+
