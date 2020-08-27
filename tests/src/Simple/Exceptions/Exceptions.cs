8066e7140fb2e3cfff1f6468e5dfbec65225df9c
Scott Waye <scott.waye@hubse.com>
2020-08-27 10:33:47 -0500

    enable simple exceptions test

--- tests/src/Simple/Exceptions/Exceptions.cs
+++ tests/src/Simple/Exceptions/Exceptions.cs
@@ -3,6 +3,10 @@
 
 using System;
 using System.Text;
+#if CODEGEN_WASM
+using System.Runtime.InteropServices;
+using Console=BringUpTest.Console;
+#endif
 
 public class BringUpTest
 {
@@ -198,5 +202,45 @@ public class BringUpTest
         CreateSomeGarbage();
         return true;
     }
+
+#if CODEGEN_WASM
+    internal class Console
+    {
+        private static unsafe void PrintString(string s)
+        {
+            int length = s.Length;
+            fixed (char* curChar = s)
+            {
+                for (int i = 0; i < length; i++)
+                {
+                    TwoByteStr curCharStr = new TwoByteStr();
+                    curCharStr.first = (byte)(*(curChar + i));
+                    printf((byte*)&curCharStr, null);
+                }
+            }
+        }
+
+        internal static void WriteLine(string s)
+        {
+            PrintString(s);
+            PrintString("\n");
+        }
+
+        internal static void WriteLine(string format, string p)
+        {
+            PrintString(string.Format(format, p));
+            PrintString("\n");
+        }
+    }
+
+    struct TwoByteStr
+    {
+        public byte first;
+        public byte second;
+    }
+
+    [DllImport("*")]
+    private static unsafe extern int printf(byte* str, byte* unused);
+#endif
 }
 
